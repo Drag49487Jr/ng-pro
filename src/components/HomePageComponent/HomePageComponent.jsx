e05c8e0ba448c23d69bbfbec9510e883a77c38a6
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-12-14 17:12:22 -0600

    add homepage component

---
+++ src/components/HomePageComponent/HomePageComponent.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class HomePageComponent extends Component {
+    render() {
+        return(
+            <div>Home Page</div>
+        )
+    }
+}
+
+export default HomePageComponent;
